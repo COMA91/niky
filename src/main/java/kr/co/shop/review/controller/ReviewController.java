178fb2f5c1740c7549a76bb20a038d164dcc1b9d
COMA <121143062+COMA91@users.noreply.github.com>
2023-01-09 17:10:23 +0900

    Add files via upload

---
+++ src/main/java/kr/co/shop/review/controller/ReviewController.java
@@ -0,0 +1,19 @@
+package kr.co.shop.review.controller;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.co.shop.review.service.ReviewService;
+
+
+
+@Controller
+@RequestMapping("/review")
+public class ReviewController {
+	
+	@Inject
+	private ReviewService rSerivce;
+
+}
